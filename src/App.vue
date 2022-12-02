ae9e99147b15ea7382494d74584bbe8329daa55a
heart <7362469@qq.com>
2022-12-03 02:13:19 +0800

    fix: fix button style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
-<template></template>
+<template>
+  <Dict />
+  <Button>123</Button>
+  <Button type="dashed">123</Button>
+  <Button type="primary">123</Button>
+  <Button type="danger">123</Button>
+  <Button type="flat">123</Button>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Dict from '@/pages/dict/index.vue'
+import { Button } from './components'
+</script>
 
 <style></style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
+import './style.scss'
 import App from './App.vue'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 import { createPinia } from 'pinia'
 import type { App as AppInterFace } from 'vue'
 const app = createApp(App)
